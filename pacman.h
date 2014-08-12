c71b1b2c2b465205b76db4d63dbe2247331b3fe9
Alexandre Dantas <eu@alexdantas.net>
2014-08-12 13:31:55 -0400

    Imported Upstream version 1.3

--- pacman.h
+++ pacman.h
@@ -1,7 +1,12 @@
 // Some variables that you may want to change
-char LevelFile[50] = "/usr/local/share/pacman/Levels/level__.dat";	//Locations of default levels
-int FreeLife = 1000;							//Starting points for free life
-int Points = 0;								//Initial points
-int Lives = 3;								//Number of lives you start with
-int HowSlow = 3;							//How slow vulnerable ghost move
-int SpeedOfGame = 175;							//How much of a delay is in the game
+
+#ifndef DATAROOTDIR
+#    define DATAROOTDIR "/usr/local/share"
+#endif
+#define LEVELS_FILE DATAROOTDIR "/pacman/Levels/level__.dat"
+char LevelFile[] = LEVELS_FILE;     //Locations of default levels
+int FreeLife = 1000;                //Starting points for free life
+int Points = 0;                     //Initial points
+int Lives = 3;                      //Number of lives you start with
+int HowSlow = 3;                    //How slow vulnerable ghost move
+int SpeedOfGame = 175;              //How much of a delay is in the game
